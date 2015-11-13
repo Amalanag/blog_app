b3c0fc1d1cac15fc4de27106bafb46a11321c3f1
Amala <amalarails@gmail.com>
2015-11-13 20:39:53 +0000

    Setup user sign in

---
+++ spec/features/signing_users_in_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature "Users sign-in" do
+
+	before do
+		@amala=User.create!(email: "amalarails@gmail.com", password: "123456789")
+		end
+
+	scenario "with valid credentials" do
+	  visit "/"
+
+	click_link "Sign in"
+	fill_in "Email", with: @amala.email
+	fill_in "Password", with: @amala.password
+	click_button "Log in"
+
+	expect(page).to have_content("Signed in successfully.")
+	expect(page).to have_content("Signed in as #{@amala.email}")
+
+ end
+end
