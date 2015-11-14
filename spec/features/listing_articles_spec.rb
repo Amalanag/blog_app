6a5cf332a6afe57f45a9ad6b659d4e7394bc3535
Amala <amalarails@gmail.com>
2015-11-14 06:27:20 +0000

    restrict new edit delete options for signed in users and owners

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -8,12 +8,14 @@ RSpec.feature "Listing Articles" do
   end
   scenario "List all articles" do
     visit "/"
-    
+
     expect(page).to have_content(@article1.title)
     expect(page).to have_content(@article1.body)
     expect(page).to have_content(@article2.title)
     expect(page).to have_content(@article2.body)
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
+		expect(page).not_to have_link("New Article")
+
   end
 end
--- spec/features/showing_article_spec.rb
+++ spec/features/showing_article_spec.rb
@@ -1,19 +1,55 @@
 require 'rails_helper'
 
 RSpec.feature "Showing an Article" do
-  
+
   before do
-    @article = Article.create(title:"The first article", body: "Body of first article")
+		@amala = User.create(email:"amalarails@gmail.com",password:"123456789")
+		@article = Article.create(title:"The first article", body: "Body of first article",user: @amala)
   end
-  
+	scenario "A non-signed in user does not see Edit and Delete links" do
+		visit "/"
+
+		click_link @article.title
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+
+		expect(page).not_to have_link("Edit Article")
+		expect(page).not_to have_link("Delete Article")
+	end
+
+		scenario "A non-owner signed in cannot see Edit and Delete links" do
+
+		  login_as(@amala)
+			visit "/"
+			click_link @article.title
+
+		expect(page).not_to have_link("Edit Article")
+		expect(page).not_to have_link("Delete Article")
+
+		end
+
+	scenario "A signed in owner can see Edit and Delete links" do
+     login_as(@amala)
+		
+			visit "/"
+		
+			click_link @article.title
+
+		expect(page).to have_link("Edit Article")
+		expect(page).to have_link("Delete Article")
+
+		end
+
+
   scenario "Display individual article" do
     visit "/"
-    
+
     click_link @article.title
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
     expect(current_path).to eq(article_path(@article))
-    
+
   end
-  
+
 end
